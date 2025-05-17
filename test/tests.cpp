ffa9c3979ea9a541eb2289d21a83990c2a5ffd40
Anton A. Shtanyuk <ashtanyuk@gmail.com>
2025-05-17 20:09:08 +0300

    Initial commit

---
+++ test/tests.cpp
@@ -0,0 +1,31 @@
+// Copyright 2025 NNTU-CS
+#include <gtest/gtest.h>
+#include <string>
+
+#include "bst.h"
+BST<std::string> makeTree(const char* filename);
+
+TEST(lab8, test1_1) {
+    const char* filename = "/home/runner/work/ADS-8/ADS-8/build/test/war_peace.txt";
+    BST<std::string> tree = makeTree(filename);
+    int depth = tree.depth();
+    EXPECT_EQ(depth, 35);
+}
+TEST(lab8, test1_2) {
+    const char* filename = "/home/runner/work/ADS-8/ADS-8/build/test/war_peace.txt";
+    BST<std::string> tree = makeTree(filename);
+    int count = tree.search("pierre");
+    EXPECT_EQ(count, 1963);
+}
+TEST(lab8, test1_3) {
+    const char* filename = "/home/runner/work/ADS-8/ADS-8/build/test/war_peace.txt";
+    BST<std::string> tree = makeTree(filename);
+    int count = tree.search("natasha");
+    EXPECT_EQ(count, 1212);
+}
+TEST(lab8, test1_4) {
+    const char* filename = "/home/runner/work/ADS-8/ADS-8/build/test/war_peace.txt";
+    BST<std::string> tree = makeTree(filename);
+    int count = tree.search("andrew");
+    EXPECT_EQ(count, 1143);
+}
